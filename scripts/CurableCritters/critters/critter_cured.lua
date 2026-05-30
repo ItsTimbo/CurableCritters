e3fd9bc5423c675d9957875796284c48053aae14
Timbo <timsdatascienceproject@gmail.com>
2026-05-31 00:48:12 +0200

    added groundwork for critter feeding

--- scripts/CurableCritters/critters/critter_cured.lua
+++ scripts/CurableCritters/critters/critter_cured.lua
@@ -1,11 +1,14 @@
 -- Declarations --
 local AI = require("openmw.interfaces").AI
 local nearby = require("openmw.nearby")
+local self = require("openmw.self")
+local core = require("openmw.core")
+local types = require("openmw.types")
 
 -- start follower AI
 local function loadFriendlyCritterBehavior()
     AI.startPackage({
-        type='Follow',
+        type = 'Follow',
         cancelOther = true,
         target = nearby.players[1],
         duration = 60,
@@ -13,8 +16,40 @@ local function loadFriendlyCritterBehavior()
     })
 end
 
+local function onActivated(actor)
+    core.sendGlobalEvent('checkFeed', {
+        critter = self.object,
+        player = actor
+    })
+end
+
+local function calculateHealed(isFavorite, feedQuality)
+    local baseHealth = 3
+    local favoriteMult = 1
+
+    if isFavorite then
+        favoriteMult = 1.1
+    end
+
+    local heal = baseHealth * favoriteMult * feedQuality
+
+    return heal
+end
+
+local function satisfyCritter(data)
+    local health = types.Actor.stats.dynamic.health(self)
+    local maxHP = health.base + health.modifier
+    local healed = calculateHealed(data.isFavorite, data.feedQuality)
+    health.current = math.min(maxHP, health.current + healed)
+    print('current HP: '..health.current..' | maxHP: '.. maxHP .. ' | healed: ' .. healed)
+end
+
 return {
     eventHandlers = {
-        loadFriendlyCritterBehavior = loadFriendlyCritterBehavior
+        loadFriendlyCritterBehavior = loadFriendlyCritterBehavior,
+        satisfyCritter = satisfyCritter,
+    },
+    engineHandlers = {
+        onActivated = onActivated
     }
 }
--- scripts/CurableCritters/global.lua
+++ scripts/CurableCritters/global.lua
@@ -1,10 +1,16 @@
 -- Declarations --
 local world = require("openmw.world")
 local util = require("openmw.util")
+local types = require("openmw.types")
 
 local infectedCritterScript = 'scripts/CurableCritters/critters/critter_infected.lua'
 local curedCritterScript = 'scripts/CurableCritters/critters/critter_cured.lua'
 
+local feedItems = {
+    mudcrab_cured = 'curablecritters_mudcrab_feed_1',
+    critter_feed_2 = "blooms",
+    critter_feed_3 = "goldies",
+}
 
 -- Event Handlers --
 local function infectCritters(infectedCritters)
@@ -38,10 +44,36 @@ local function cureCritter(critter)
     curedCritter:sendEvent('loadFriendlyCritterBehavior')
 end
 
+local function checkFeed(data)
+    local feedSuccess = {
+        isFavorite = false,
+        feedQuality = 0,
+    }
+    for _, item in pairs(types.Actor.getEquipment(data.player)) do
+        for feedType, itemName in pairs(feedItems) do
+            -- check if Player is holding feed when interacting
+            -- and check if feed is for correct critter
+            if item.recordId == itemName then
+                print(data.critter.recordId)
+                if feedType == data.critter.recordId then
+                    print(data.critter.recordId)
+                    data.critter:sendEvent('satisfyCritter', {
+                        isFavorite = true,
+                        feedQuality = 1.1,
+                    })
+                else
+                    data.critter:sendEvent('satisfyCritter', feedSuccess)
+                end
+            end
+        end
+    end
+end
+
 -- Return
 return {
     eventHandlers = {
         infectCritters = infectCritters,
         cureCritter = cureCritter,
+        checkFeed = checkFeed,
     }
 }
